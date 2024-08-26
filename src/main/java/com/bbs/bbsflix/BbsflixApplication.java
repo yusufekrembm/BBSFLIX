5d5f653960507fea4c1ad772e65045a2e5bed281
Yusuf Ekrem Ünlü <ekrem.unlu@bilgibirikim.com>
2024-08-26 13:29:33 +0300

    Project initalized

---
+++ src/main/java/com/bbs/bbsflix/BbsflixApplication.java
@@ -0,0 +1,13 @@
+package com.bbs.bbsflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsflixApplication.class, args);
+    }
+
+}
