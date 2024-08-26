d9ac3cfcfe08cb618b6d39cc8d718296e9c116fd
egemen6161 <egemenkaraman7161@gmail.com>
2024-08-26 14:20:32 +0300

    Entity Yapısı Oluşturuldu

---
+++ src/main/java/com/bbs/bbsflix/MovieEntity.java
@@ -0,0 +1,49 @@
+package com.bbs.bbsflix;
+
+import java.util.List;
+
+public class MovieEntity {
+    private int page;
+    private List<ResultsEntity> results;
+    private int total_pages;
+    private int total_results;
+
+    public MovieEntity(int page, List<ResultsEntity> results, int total_pages, int total_results) {
+        this.page = page;
+        this.results = results;
+        this.total_pages = total_pages;
+        this.total_results = total_results;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<ResultsEntity> getResults() {
+        return results;
+    }
+
+    public void setResults(List<ResultsEntity> results) {
+        this.results = results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+}
