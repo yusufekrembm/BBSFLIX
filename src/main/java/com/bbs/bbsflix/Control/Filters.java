e75b74a9901166259a193bb4c7a357af0729fbb4
Emre Cem Kenarcı <emrecemkenarci@icloud.com>
2024-08-27 09:28:43 +0300

    eski src dosyası kopyalandı.

---
+++ src/main/java/com/bbs/bbsflix/Control/Filters.java
@@ -0,0 +1,29 @@
+package com.bbs.bbsflix.Control;
+
+import java.util.List;
+
+public class Filters {
+
+    public String filterById(){
+        return "doldurulucak";
+    }
+
+    public String filterByName(String title){
+        return "doldurulucak";
+    }
+
+    public List filterByGenres(List<Integer> genre_ids){
+        return List.of();
+    }
+
+    public List filterMovies(/* BİR PARAMETRE GELİCEK */){
+        return List.of();
+    }
+
+    public List filterByLanguage(List<String> languages){
+        return List.of();
+    }
+
+
+
+}
