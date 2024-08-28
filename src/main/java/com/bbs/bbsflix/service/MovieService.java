5d78e552a243004e8c2f4aca3ef6d5c6ba9d0fd7
Emre Cem Kenarcı <emrecemkenarci@icloud.com>
2024-08-28 09:27:04 +0300

    gite göndermeme sorununun çözüm denemesi

--- src/main/java/com/bbs/bbsflix/service/MovieService.java
+++ src/main/java/com/bbs/bbsflix/service/MovieService.java
@@ -31,7 +31,7 @@ public class MovieService {
 
     public MovieEntity getMovies() throws IOException {
 
-        String url = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&language=en-US&page=10";
+        String url = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&language=en-US&page=11";
         String response = restTemplate.getForObject(url, String.class);
 
         Map<String, Object> responseMap = objectMapper.readValue(response, new TypeReference<Map<String, Object>>(){});
@@ -62,3 +62,9 @@ public class MovieService {
         return Order.orderByPopularity(movies);
     }
 }
+
+
+/*
+{2,3,4},{1,5,6}
+
+ */
