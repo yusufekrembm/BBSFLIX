63967c9855c179f96abd7fd2135c627148bdf73e
Emre Cem Kenarcı <emrecemkenarci@icloud.com>
2024-08-28 13:53:24 +0300

    eski loga dönüldü, hatalar düzeltildi.

--- src/main/java/com/bbs/bbsflix/Control/MovieControl.java
+++ src/main/java/com/bbs/bbsflix/Control/MovieControl.java
@@ -112,6 +112,6 @@ public class MovieControl {
 
 
 
-    }
-
 }
+
+
--- src/main/java/com/bbs/bbsflix/service/MovieService.java
+++ src/main/java/com/bbs/bbsflix/service/MovieService.java
@@ -1,6 +1,9 @@
 package com.bbs.bbsflix.service;
 
+import com.bbs.bbsflix.model.MovieEntity;
 import com.bbs.bbsflix.model.ResultsEntity;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +14,21 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MovieService {
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     @Value("${tmdb.api.key}")
     private String apiKey;
 
     @Autowired
-    public MovieService(RestTemplate restTemplate) {
+    public MovieService(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
     }
 
     public MovieEntity getMovies() throws IOException {
