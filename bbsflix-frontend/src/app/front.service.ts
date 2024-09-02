86061e56f987bf629ebb16442e25bf9122aab777
Emre Cem Kenarcı <emrecemkenarci@icloud.com>
2024-09-02 16:47:49 +0300

    dile göre filtre eklendi, test edidli. birden fazla filtre çalışıyor.

--- bbsflix-frontend/src/app/front.service.ts
+++ bbsflix-frontend/src/app/front.service.ts
@@ -6,30 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FrontService {
-  private baseUrl = 'http://localhost:8080/api/bbsflix';
-
-  private readonly GENRE_IDS: { [key: string]: number } = {
-    'Action': 28,
-    'Adventure': 12,
-    'Animation': 16,
-    'Comedy': 35,
-    'Crime': 80,
-    'Documentary': 99,
-    'Drama': 18,
-    'Family': 10751,
-    'Fantasy': 14,
-    'History': 36,
-    'Horror': 27,
-    'Music': 10402,
-    'Mystery': 9648,
-    'Romance': 10749,
-    'Science Fiction': 878,
-    'TV Movie': 10770,
-    'Thriller': 53,
-    'War': 10752,
-    'Western': 37,
-  };
-  
+  private baseUrl = 'http://localhost:8080/api/bbsflix';  
 
   constructor(private http: HttpClient) { }
 
@@ -43,20 +20,26 @@ export class FrontService {
   
   filterAndOrderMovies(filters: any): Observable<any> {
     let params = new HttpParams()
-      .set('title', filters.title || '')
-      .set('sortBy', 'title') // Default sorting, you might want to make this dynamic
-      .set('ascending', 'true'); // Default sorting order, you might want to make this dynamic
+      .set('sortBy', filters.sortBy || 'title') 
+      .set('ascending', filters.ascending !== undefined ? filters.ascending.toString() : 'true');
   
-    // Add dynamic filter parameters
-    if (filters.genreId) {
-      params = params.set('genreId', filters.genreId); // Ensure the key is 'genreId'
+    if (filters.title) {
+      params = params.set('title', filters.title);
     }
   
-    // Log the URL for debugging
+    if (filters.genreId) {
+      params = params.set('genreId', filters.genreId);
+    }
+  
+    if (filters.language) {
+      params = params.set('language', filters.language);
+    }
+  
     console.log('Request URL:', `${this.baseUrl}/filterAndOrderMovies?${params.toString()}`);
-    
+  
     return this.http.get(`${this.baseUrl}/filterAndOrderMovies`, { params });
   }
+  
   
   
   
